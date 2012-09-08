3062a72948e7756c1c58b91e65ed7a4da4a0a472
n.showket@gmail.com <n.showket@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-08 23:06:29 +0000

    fancy new feature
    git-svn-id: https://plugins.svn.wordpress.org/simple-e-commerce-shopping-cart/trunk@596422 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/more.php
@@ -0,0 +1,48 @@
+<div style="margin-right:10px;margin-top:10px; padding:5px;background-color : #FCFCFC;border:solid 1px #DFDFDF;">
+	 <h3 style="margin:0;padding:0;">Need More Features?</h3>
+	 <ul style="list-style-type:disc;margin-left:20px;">
+		<li>Deliver Downloads from "Thank You" page </li>
+		<li>Automatic Link Expiration </li>
+		<li>Regenerate Download Link </li>
+		<li>Multiple Files Upload per Product </li>
+		<li>Product Bundling </li>
+		<li>License Key Generation </li>
+		<li>Allow Free Downloads </li>
+		<li>Amazon S3 Integration </li>
+		<li>Autoresponder Integration  (Aweber, MailChimp, GetResponse) </li>
+		<li>iPhone Application </li>
+		<li>and more.. </li>
+	 </ul>
+	 <div style="text-align: center;">
+		<a target="_blank" href="http://www.wpbizcart.com"> <img  src="<?php echo BIZCART_BOX_1_IMAGE?>"/>
+		 </a>
+		<h4>WPBizcart Single License $29</h4>
+		
+		<a target="_blank" class="button-primary" href="http://www.wpbizcart.com/">Buy Now
+		</a>
+	</div>
+	<br/>
+	<br>
+	
+</div>																								
+	<div style="margin-right:10px;margin-top:10px; padding:5px;background-color : #FCFCFC;border:solid 1px #DFDFDF;">
+		<h3 style="margin:0;padding:0;">Get Support</h3>
+		<ul>
+			<li> 
+				<img src="<?php echo HELP_IMAGE?>"> 
+				<a target="_blank" href="http://www.wpbizcart.com/support/">Documentation</a>
+			</li>
+			<li> 
+				<img src="<?php echo TABLE_IMAGE?>"> 
+				<a target="_blank" href="http://wordpress.org/support/plugin/simple-e-commerce-shopping-cart">Support Forums</a>
+			</li>
+			<li> 
+				<img src="<?php echo BUG_IMAGE?>"> 
+				<a target="_blank" href="http://www.wpbizcart.com/contact/">Report a Bug</a>
+			</li>
+			<li> 
+				<img src="<?php echo HELP_IMAGE?>"> 
+				<a target="_blank" href="http://www.wpbizcart.com/contact/">Presale Question</a>
+			</li>
+		</ul>
+	</div>		
