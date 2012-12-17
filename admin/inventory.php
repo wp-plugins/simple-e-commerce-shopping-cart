97f67a439a6890f7f33656ada87309cf56465cf5
n.showket@gmail.com <n.showket@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-17 10:35:22 +0000

    Adding my links
    git-svn-id: https://plugins.svn.wordpress.org/simple-e-commerce-shopping-cart/trunk@640447 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/inventory.php
+++ admin/inventory.php
@@ -1,2 +1,2 @@
- <h2 style="font-weight:normal;">Inventory Tracking and Notification are only available in <a target="_blank" href="http://www.wpbizcart.com/">WP Bizcart</a> (Simple eCommerce premium version)Inventory features such as Bulk Product price and inventory Update, Stock control and Notifications are available in Simple eCommerce Premium Version. </h2> 
+ <h2 style="font-weight:normal;">Inventory Tracking and Notification are only available in <a target="_blank" href="http://www.wordpressshopcart.com/">WP Shop Cart</a> (Simple eCommerce premium version)Inventory features such as Bulk Product price and inventory Update, Stock control and Notifications are available in Simple eCommerce Premium Version. </h2> 
 
--- admin/orders.php
+++ admin/orders.php
@@ -1,3 +1,3 @@
  
-<h2  style="font-weight:normal;">Sales Orders are only available in  <a target="_blank" href="http://www.wpbizcart.com/">WP Bizcart</a> (Simple eCommerce premium version) </h2>
+<h2  style="font-weight:normal;">Sales Orders are only available in  <a target="_blank" href="http://www.wordpressshopcart.com/">WP shop cart</a> (Simple eCommerce premium version) </h2>
  
