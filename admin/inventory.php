972d39202468421532c74f0856b210635ef0209d
n.showket@gmail.com <n.showket@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-20 10:45:56 +0000

    adding my new site
    git-svn-id: https://plugins.svn.wordpress.org/simple-e-commerce-shopping-cart/trunk@728840 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/inventory.php
+++ admin/inventory.php
@@ -1,2 +1,1 @@
- <h2 style="font-weight:normal;">Inventory Tracking and Notification are only available in the premium version call ed wp_shop_cart.Inventory features such as Bulk Product price and inventory Update, Stock control and Notifications are available in Simple eCommerce Premium Version. Please email n.showket@gmail.com for more details</h2> 
-
+ <h2 style="font-weight:normal;">Inventory Tracking and Notification are only available in the premium version call ed wp_shop_cart.Inventory features such as Bulk Product price and inventory Update, Stock control and Notifications are available in Simple eCommerce Premium Version.Please visit http://smartwpplugin.com for more details.</h2>
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 === WordPress Simple Ecommerce Shopping Cart Plugin- Sell products through Paypal ===
 Contributors: Niaz Showket
-Donate link:
+Donate link: http://smartwpplugin.com
 Tags: Wordpress shopping cart, wordpress shopping cart plugin, shopping cart, cart, checkout, shop, store, wordpress ecommerce, ecommerce plugin, e-commerce plugin, digital downloads, paypal, paypal shopping cart, paypal cart, online shop, online store
 Requires at least: 1.0
 Tested up to: 3.5
