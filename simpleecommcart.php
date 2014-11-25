f2522a875dde4402da7a32873c451648b4c4c46b
n.showket@gmail.com <n.showket@gmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-25 11:21:50 +0000

    Upgrade for version 4
    git-svn-id: https://plugins.svn.wordpress.org/simple-e-commerce-shopping-cart/trunk@1032455 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simpleecommcart.php
+++ simpleecommcart.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple eCommerce
 Plugin URI: http://wordpress.org/
 Description: Transform your wordpress site into an online shop and sell products or services with Simple eCommerce shopping cart plugin. Plugin includes Cart Widget, shortcodes and email tags to customise your shop.
-Version: 2.2.2
+Version: 2.2.3
 Author: Niaz Showket
 
 ------------------------------------------------------------------------
